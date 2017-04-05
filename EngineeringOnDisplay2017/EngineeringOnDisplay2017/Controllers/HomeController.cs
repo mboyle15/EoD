608dfc6ee5026bb467f2a4f6af865465781c3f5b
thorelore <thorelore@gmail.com>
2017-04-04 21:43:21 -0800

    Basic Template for Engineering On Display with Visual Studios 2017 and Asp.net Core

---
+++ EngineeringOnDisplay2017/EngineeringOnDisplay2017/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿/****************************************************************************************************************
+ * Project: Engineering on Display
+ * Purpose: Website for displaying sensor data from the Engineering and Industry Building at the University of
+ *          Alaska, Anchorage.  First features is a front end website for the general users to view
+ *          the graphical data of the sensors and read more on the purpose of the display.  Second Feature
+ *          is a admin secured website to add more slideshows to the third feature and view statistics. The third feature
+ *          is a slide show of different advertisements.  
+ * 
+ * Authors:  Martin Boyle
+ *           Terrance Mount
+ *           Andrew Smart
+ *           
+ * Sponsor: Dr. Kenrick Mock
+ * 
+ * Instructor: Dr. Martin Cenek
+ * 
+ * Class:  CSCE 470 Capstone  Spring 2017
+ * College: University of Alaska, Anchorage
+ * ***********************************************************************************************************************
+ * File: HomeController.cs
+ * Purpose: to run the home, about and contact views of the website.    
+ * 
+ * *******************************************************************************************************************
+ * Methods
+ * 
+ *  
+ *  *************************************************************************************************************
+ * Change Log
+ * 
+ * **************************************************************************************************************
+ * ToDo
+ * 
+ *  Write out puedo-code methods in the section above and create views as needed.  
+ * **************************************************************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngineeringOnDisplay2017.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
